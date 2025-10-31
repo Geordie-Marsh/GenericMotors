8952c2096b0ec480d023369c073226d6346b9f63
Geordie Marsh <mail@geordiemarsh.com>
2025-10-31 13:10:03 +1100

    Finished find game

--- src/app/find/FindClient.tsx
+++ src/app/find/FindClient.tsx
@@ -35,17 +35,27 @@ export default function Find() {
 	const gameTimeLength = 30; // seconds
 
 	// States
+	const [ isLoaded, setIsLoaded ] = useState<boolean>(false);
 	const [ difficulty, setDifficulty ] = useState<"easy" | "medium" | "hard">("hard");
 	const [ timeLeft, setTimeLeft ] = useState<number>(gameTimeLength);
 	const [ score, setScore ] = useState<number>(0);
 	// const [ gridCars, setGridCars ] = useState<StaticImageData[]>([]);
 	const [ desiredCar, setDesiredCar ] = useState<StaticImageData>(audiQ5); // Default desired car
+	const [ gameOver, setGameOver ] = useState<boolean>(false);
 
 	// Refs
+	const introContRef = useRef<HTMLDivElement | null>(null);
 	const timerRef = useRef<HTMLHeadingElement | null>(null);
 	const scoreRef = useRef<HTMLSpanElement | null>(null);
 	const findContRef = useRef<HTMLDivElement | null>(null);
 	const desiredCarRef = useRef<HTMLImageElement | null>(null);
+	const gameContRef = useRef<HTMLDivElement | null>(null);
+
+	const finishedContRef = useRef<HTMLDivElement | null>(null);
+	const finishedPreScoreRef = useRef<HTMLHeadingElement | null>(null);
+	const finishedScoreRef = useRef<HTMLHeadingElement | null>(null);
+	const finishedMessageRef = useRef<HTMLParagraphElement | null>(null);
+	const finishedButtonsContRef = useRef<HTMLDivElement | null>(null);
 
 	const carImages = [
 		audiQ5,
@@ -58,6 +68,24 @@ export default function Find() {
 		toyotaRAV4,
 		volvoXC60,
 	];
+
+
+	useEffect(() => {
+    	let loadedCount = 0;
+
+    	carImages.forEach((img) => {
+     		const image = new window.Image();
+     		image.src = img.src; // important: Next.js images are objects
+     		image.onload = () => {
+     			loadedCount++;
+     			if (loadedCount === carImages.length) {
+     				setIsLoaded(true);
+     			}
+     		};
+     	});
+ 	}, []);
+
+
 
 	// Number of cars in the grid depending on difficulty
 	const noOfCars = useMemo(() => {
@@ -75,10 +103,307 @@ export default function Find() {
 
 
 
-	function newGame() {
+
+	useEffect(() => {
+		if (!gameOver) return;
+
+		// End the game - hide cars, show final score screen
+		gameContRef.current!.style.pointerEvents = "none"; // Disable further clicks
+		gameContRef.current!.style.visibility = "hidden";
+
+
+		// Log final score
+		// console.log(`Final Score: ${score}, Rounds Played: ${roundsPlayed}, Accuracy: ${accuracy}%`);
+
+		showFinishedScreen();
+
+
+		const calculateScoreWorth = (score: number, difficulty: "easy" | "medium" | "hard") => {
+			switch (difficulty) {
+				case "easy":
+					if (score < 10) return "low";
+					else if (score < 15) return "medium";
+					else return "high";
+				case "medium":
+					if (score < 8) return "low";
+					else if (score < 12) return "medium";
+					else return "high";
+				case "hard":
+					if (score < 5) return "low";
+					else if (score < 8) return "medium";
+					else return "high";
+				default:
+					return "high";
+			}
+		};
+		const scoreWorth = calculateScoreWorth(score, difficulty);
+
+
+
+		// const finalScore = 20; // Hardcoded to 78% for demo purposes
+
+		function showFinishedScreen() {
+			const finishedCont = finishedContRef.current;
+			
+			// Make it visible
+			finishedCont!.style.visibility = "visible";
+			
+			// Animate it in
+			gsap.fromTo(
+				finishedCont,
+				{ 
+					opacity: 0 
+				},
+				{ 
+					opacity: 1, 
+					duration: 0.5,
+					onComplete: () => {
+						setTimeout(() => {
+							animateInPreScore();
+						}, 200);
+					}
+				}
+			);
+		}
+
+
+		function animateInPreScore() {
+			const finishedPreScore = finishedPreScoreRef.current;
+
+			// Animate it in
+			gsap.fromTo(
+				finishedPreScore,
+				{
+					opacity: 0
+				},
+				{
+					opacity: 1,
+					duration: 0.3,
+					onComplete: () => {
+						setTimeout(() => {
+							animateInScore();
+						}, 400);
+					}
+				}
+			)
+		}
+		
+		function animateInScore() {
+			const finishedScore = finishedScoreRef.current;
+
+			// Animate it in
+			gsap.fromTo(
+				finishedScore,
+				{
+					opacity: 0
+				},
+				{
+					opacity: 1,
+					duration: 0.3,
+					onComplete: () => {
+						setTimeout(() => {
+							animateScoreCountUp();
+						}, 100);
+					}
+				}
+			)
+		}
+		
+		
+		function animateScoreCountUp() {
+			const finishedScore = finishedScoreRef.current;
+
+			// Animation parameters
+			const duration = 2.3;
+
+			// Animate it in (scale)
+			gsap.to(
+				finishedScore,
+				{
+					scale: 1,
+					duration: duration,
+					//ease: "back.out(6)"
+					// ease: CustomEase.create("custom", "cubic-bezier(.28,-0.29,.55,1.88)")
+					ease: CustomEase.create("custom", "M0,0 C0.087,0 0.18,-0.16 0.503,-0.16 0.829,-0.16 0.81,1.238 0.83,1.602 0.858,1.284 0.922,1 1,1 "),
+				}
+			)
+		
+			// Animate it in (count up)
+			gsap.to(
+				finishedScore,
+				{
+					innerText: `${ score }`,
+					duration: duration,
+					ease: CustomEase.create("custom", "M0,0 C0.03,0 0.136,0 0.164,0 0.353,0 0.46,0.05 0.527,0.127 0.6,0.21 0.686,0.362 0.727,0.501 0.769,0.646 0.826,1.001 0.826,1.001 0.826,1.001 0.884,1 1,1 "),
+					snap: {
+						innerText: 1
+					}
+				}
+			);
+
+			// Change the colour depending on the score (which also depends on the difficulty)
+			const scoreColor = scoreWorth === "low" ? "#ff505b" // Red
+				: scoreWorth === "medium" ? "#fbbf24" // Yellow
+				: "#22c55e"; // Green
+
+			setTimeout(() => {
+				finishedScore!.style.color = scoreColor;
+			}, duration * 1000 * 0.829);
+
+			// After the count up is done, animate in the messages
+			setTimeout(() => {
+				animateInMessages();
+			}, duration * 1000 + 300);
+		}
+
+
+		function animateInMessages() {
+			// const finishedAccuracy = finishedAccuracyRef.current;
+			const finishedMessage = finishedMessageRef.current;
+
+
+
+			const finalMessage =
+				scoreWorth === "high" && difficulty === "hard" ? "Wow! You've got a keen eye! Very impressive."
+				: scoreWorth === "high" ? "Wow! You've got a keen eye! Wanna try a difficulty up?" 
+				: scoreWorth === "medium" ? `Good score, but let's see if you can get into the green zone!`
+				: score <= 0 ? "Yikes... that didn't go well. Try your luck again?"
+				: `Come on, you can do better than ${score}! Let's try again!`;
+
+
+			// // Type in the accuracy message letter by letter
+			// gsap.to(finishedAccuracy, {
+			// 	duration: accuracyMessage.length * 0.02,
+			// 	text: accuracyMessage,
+			// 	ease: "power1.in"
+			// });	
+
+			// Type in the final message letter by letter
+			gsap.to(finishedMessage, {
+				text: finalMessage,
+				ease: "power1.in",
+				duration: finalMessage.length * 0.02,
+				// delay: accuracyMessage.length * 0.02 + 0.8,
+				onComplete: () => {
+					// After all messages are done, show the buttons
+					setTimeout(() => {
+						animateInButtons();
+					}, 800);
+				}
+			});
+		}
+
+
+		function animateInButtons() {
+			const finishedButtonsCont = finishedButtonsContRef.current;
+			if (!finishedButtonsCont) return;
+
+			// Animate in the buttons 
+			// Get the buttons inside the buttons container
+			const buttons = Array.from(finishedButtonsCont!.children);
+			// Make the buttons visible
+			buttons.forEach((button) => {
+				(button as HTMLElement).style.visibility = "visible";
+			});
+
+			gsap.fromTo(buttons, {
+				opacity: 0,
+			}, {
+				opacity: 1,
+				duration: 0.3, 
+				stagger: 0.2,
+			});
+		}
+
+
+	}, [gameOver]);
+
+
+
+
+
+
+	function newGame(difficultyLevel: "easy" | "medium" | "hard" = "easy") {
 		// Reset score and timer
 		setScore(0);
 		setTimeLeft(gameTimeLength);
+		setDifficulty(difficultyLevel);
+		// Make the timer black again
+		if (timerRef.current) {
+			timerRef.current.style.color = "#181818";
+		}
+
+
+
+		// Animate out the intro container
+		const introCont = introContRef.current;
+		if (introCont) {
+			gsap.to(introCont, {
+				opacity: 0,
+				duration: 0.5,
+				onComplete: () => {
+					introCont.remove();
+
+					preGame();
+				}
+			});
+		}
+
+		function preGame() {
+			// Start the pre-game countdown
+			let countdown = 3;
+
+			const countdownInterval = setInterval(() => {
+				setTimeLeft(countdown);
+
+				// Show the countdown
+				timerRef.current!.style.visibility = "visible";
+
+				if (countdown <= 0) {
+					clearInterval(countdownInterval);
+					// Start the game
+					startGame();
+					return;
+				}
+				countdown--;
+			}, 1000);
+		}
+
+		function startGame() {
+			// Show the cars
+			gameContRef.current!.style.visibility = "visible";
+			
+
+			// Set the timer
+			setTimeLeft(gameTimeLength);
+
+			// Start the timer countdown
+			// Clear any existing timer
+			const timerInterval = setInterval(() => {
+				setTimeLeft((prevTime) => {
+					// When the time reaches 5, turn the timer red
+					if (prevTime === 6) {
+						timerRef.current!.style.color = "#ff505b";
+					}
+
+					if (prevTime <= 1) {
+						clearInterval(timerInterval);
+						
+						// End the game
+						setGameOver(true);
+						
+						return 0;
+					}
+					return prevTime - 1;
+				});
+			}, 1000);
+		}
+
+
+
+
+
+		
 
 		// Pick a new desired car
 		const newDesiredCar = carImages[Math.floor(Math.random() * carImages.length)];
@@ -184,6 +509,21 @@ export default function Find() {
 	}
 	
 
+	if (!isLoaded) {
+    	return (
+    		<div style={{
+        		display: "flex",
+        		justifyContent: "center",
+        		alignItems: "center",
+        		height: "100vh"
+     		}}>
+       			<p>Loading images...</p>
+      		</div>
+    	);
+  	}
+
+
+
   return (
 	<div className={styles.findCont} ref={ findContRef }>
 		<header>
@@ -212,7 +552,7 @@ export default function Find() {
 			</div>
 		</div>
 
-		<div className={ styles.gameCont }>
+		<div className={ styles.gameCont } ref={ gameContRef }>
 			{/* Show a grid of car images */}
 			{/* Assign the class to the grid depending on the difficulty */}
 			<div className={ `${styles.carGrid} ${styles[`carGrid--${difficulty}`]}` }>
@@ -224,6 +564,68 @@ export default function Find() {
 						<Image src={ gridCars[index] } alt={`Car ${index + 1}`} className={ styles.carImage } />
 					</div>
 				))}
+			</div>
+		</div>
+
+
+
+
+		<div className={ styles.introCont } ref={ introContRef }>
+			<h2>Same / Different</h2>
+			<p>See how well you can tell apart our models.<br /><br />You&apos;ll be shown two cars and you have to identify if they&apos;re the same or different. Try to get as many right as you can within the time limit!</p>
+			<div className={ styles.buttonsCont }>
+				<Button.Large className={ styles.beginButton } onClick={ () => {
+					// Start the trivia with the function
+					setTimeout(() => {
+						newGame("easy");
+					}, 300);
+				} }>
+					<h3>Easy mode</h3>
+				</Button.Large>
+				<Button.Large className={ styles.beginButton } onClick={ () => {
+					// Start the trivia with the function
+					setTimeout(() => {
+						newGame("medium");
+					}, 300);
+				} }>
+					<h3>Medium mode</h3>
+				</Button.Large>
+				<Button.Large className={ styles.beginButton } onClick={ () => {
+					// Start the trivia with the function
+					setTimeout(() => {
+						newGame("hard");
+					}, 300);
+				} }>
+					<h3>Hard mode</h3>
+				</Button.Large>
+			</div>
+		</div>
+
+
+
+
+		<div className={ styles.finishedCont } ref={ finishedContRef }>
+			<div className={ styles.finalScoreCont }>
+				<h3 className={ styles.finishedPreScore } ref={ finishedPreScoreRef }>You scored...</h3>
+				<h1 className={ styles.finishedScore } ref={ finishedScoreRef }>0</h1>
+			</div>
+			{/* <p className={ styles.finishedAccuracy } ref={ finishedAccuracyRef }></p> */}
+			<p className={ styles.finalMessage } ref={ finishedMessageRef }></p>
+			<div className={ styles.interactionsCont }>
+				<div className={ styles.buttonsCont } ref={ finishedButtonsContRef }>
+					<Button.Large className={ styles.retryButton } onClick={ () => {
+						// Reset the trivia to try again
+						window.location.reload();
+					} }>
+						<h3>Try again</h3>
+					</Button.Large>
+					<Button.Large className={ styles.homeButton } onClick={ () => {
+						// Go back to home page
+						window.location.href = "/";
+					} }>
+						<h3>More games</h3>
+					</Button.Large>
+				</div>
 			</div>
 		</div>
 	</div>
